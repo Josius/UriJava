12a4d91caba679c90dc0a37dcbe8787dc6e75d35
Josius <josius.amaro@gmail.com>
2020-03-04 16:24:23 -0300

    ede

--- Main.java
+++ Main.java
@@ -6,7 +6,23 @@ public class Main {
 
     public static void main(String[] args){
 
+		Scanner ent = new Scanner(System.in);
+		int diaIn, hrIn, minIn, segIn, diaFi, hrFin, minFi, segFi;
 		
-        
+		//System.out.printf("Dia %d",  diaIn = ent.nextInt());
+        System.out.print("Dia ");
+		diaIn = ent.nextInt();
+		hrIn = ent.nextInt();
+		minIn = ent.nextInt();
+		segIn = ent.nextInt();
+		/*
+		System.out.print("Dia ");
+		diaFi = ent.nextInt();
+		hrFin = ent.nextInt();
+		minFi = ent.nextInt();
+		segFi = ent.nextInt();
+		*/
+		double	minTot1, minTot2;
+		minTot1 = ((diaFi-diaIn)*24) + (hrIn*60) + 
     }
 }
