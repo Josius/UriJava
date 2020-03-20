57e475cab86788c5f8bef6f6b62c4ba7299092e6
Josius <josius.amaro@gmail.com>
2020-03-20 13:16:40 -0300

    mm

--- Main.java
+++ Main.java
@@ -5,9 +5,19 @@ public class Main {
     public static void main(String[] args){
 		
 		Scanner ent = new Scanner(System.in);
-		
-		
-		
+		double nota1, nota2, media;
+		boolean flag = true;
+		do{
+			nota1 = ent.nextDouble();
+			nota2 = ent.nextDouble();
+			if((nota1<0 || nota1>10) || (nota2<0 || nota2>10)){
+				System.out.println("nota invalida");
+			}else{
+				media = (nota1+nota2)/2;
+				System.out.printf("media = %.2f\n", media);
+				flag = false;
+			}
+		}while(flag == true);
 		
 	}
 }
