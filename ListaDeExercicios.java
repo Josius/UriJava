70464c63c18d93abb5ed7fe270a27b454e82be08
Josius <josius.amaro@gmail.com>
2020-04-01 20:37:55 -0300

    o

--- ListaDeExercicios.java
+++ ListaDeExercicios.java
@@ -1807,6 +1807,102 @@ public class Main {
 		
 		//1117
 		
+		Scanner ent = new Scanner(System.in);
+		double nota1, nota2, media;
+		boolean flag = true;
+		do{
+			nota1 = ent.nextDouble();
+			if(nota1<0 || nota1>10){
+				System.out.println("nota invalida");
+			}else{
+			    flag=false;
+			}
+		}while(flag == true);
+        
+        flag = true;
+		
+		do{
+		    nota2 = ent.nextDouble();
+		    if(nota2<0 || nota2>10){
+			    System.out.println("nota invalida");
+		    }
+		    else{
+			    media = (nota1+nota2)/2;
+			    System.out.printf("media = %.2f\n", media);
+			    flag = false;
+		    }
+		}while(flag == true);
+		
+		//1117 - UMA SOLUÇÃO MELHOR
+		
+		Scanner ent = new Scanner(System.in);
+		double nota1, nota2, media;
+		for(int i=0; i<1; ){
+		    nota1 = ent.nextDouble();
+			if(nota1<0 || nota1>10){
+				System.out.println("nota invalida");
+			}else{
+			    for(i=0; i<1; ){
+			        nota2 = ent.nextDouble();
+		            if(nota2<0 || nota2>10){
+			            System.out.println("nota invalida");
+		            }
+		            else{
+			            media = (nota1+nota2)/2;
+			            System.out.printf("media = %.2f\n", media);
+			            i++;
+		            }
+			    }
+			}
+		}
+		
+		//1118 - MINHA SOLUÇÃO COM 20% DE ERRO
+		
+		Scanner ent = new Scanner(System.in);
+		double nota1, nota2, media;
+		int resp;
+		boolean flag = true;
+		
+		do{
+		    do{
+			    nota1 = ent.nextDouble();
+			    if(nota1<0 || nota1>10){
+				    System.out.println("nota invalida");
+			    }else{
+			        flag=false;
+			    }
+		    }while(flag == true);
+            
+            flag = true;
+		    
+		    do{
+		        nota2 = ent.nextDouble();
+		        if(nota2<0 || nota2>10){
+			        System.out.println("nota invalida");
+		        }
+		        else{
+			        media = (nota1+nota2)/2;
+			        System.out.printf("media = %.2f\n", media);
+			        flag = false;
+		        }
+		    }while(flag == true);
+		    
+		    do{
+		        System.out.println("novo calculo (1-sim 2-nao)");
+    		    resp = ent.nextInt();
+    		   
+    		    if(resp<1 || resp>2){
+		            flag = true;
+		        }else{
+		            flag = false;
+		        }   
+		    }while(flag == true);
+		}while(resp==1);
+		
+		
+		//1131
+		
+		
 		
 		
     }
